623340b7de7c96244d749a9cd3c88293033a8027
Gabe Ratcliff <gaberatcliff@Zoes-MacBook-Air-3.local>
2016-04-22 11:39:23 -0700

    updated server

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(express.static(path.join(__dirname, './client')));
 require('./server/config/mongoose.js');
 
 require('./server/config/routes.js')(app);
-app.listen(8000, function (){
-	console.log('Listening to 8000');
+app.set('port', (process.env.PORT || 8000));
+app.set('/', function (req, res){
+	res.send("<h1> Hello World </h1>");
 })
+app.listen(app.get('port'), function(){
+ console.log("listening on port ", app.get('port'));
+});
